35a48575de3261bb7c825a8eaebdff93890a68c7
zestze <eer2138@columbia.edu>
2018-05-23 23:57:29 -0400

    can successfully retrieve discover weekly tracks... now what to do with this info?

--- sp-main.py
+++ sp-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """
-@TODO: integrate flask support, so user can say "yes" to giving their info?
+@TODO: refresh token function
+
+@TODO: change global stuff, wrap in a class
 """
 import sys
 import requests
@@ -16,14 +18,16 @@ _USER_AGENT_ = "simpleSpotifyClient/0.1 by Zeke"
 # needs to be space separated
 SCOPES = "playlist-read-private user-top-read user-read-recently-played" \
         " user-library-read user-read-private"
-
 PORT = 8888
-
 REDIRECT_URI = "http://127.0.0.1:8888/callback"
 
 client_id = ""
 client_secret = ""
 client_is_base64 = ""
+
+REFRESH_TOKEN = ""
+ACCESS_TOKEN = ""
+TOKEN_TYPE = ""
 
 def grabCredentials():
     global client_id
@@ -35,6 +39,17 @@ def grabCredentials():
         client_is_base64 = myFile.read().replace('\n', '')
     with open('client.secret', 'r') as myFile:
         client_secret = myFile.read().replace('\n', '')
+
+def refreshToken():
+    data = {"grant_type": "refresh_token", "refresh_token": REFRESH_TOKEN}
+    headers = {"Authorization": "Basic {}".format(client_is_base64)}
+    response = requests.post("https://accounts.spotify.com/api/token",
+                             data=data, headers=headers)
+    rJson = response.json()
+    accessToken = rJson['access_token']
+    tokenType = rJson['token_type']
+    # also have 'scope' and 'expires_in' keys
+    return accessToken
 
 @app.route('/')
 def home():
@@ -50,6 +65,32 @@ def home():
 
     return flask.redirect(response.url)
 
+def getDiscoverID():
+    headers = {"Authorization": "{} {}".format(TOKEN_TYPE, ACCESS_TOKEN)}
+    response = requests.get("https://api.spotify.com/v1/me/playlists", headers=headers)
+    rJson = response.json()
+
+    playlistsList = rJson['items'] # is a list of dictionaries
+    for pJson in playlistsList:
+        if pJson['name'] == 'Discover Weekly':
+            print (pJson)
+            return pJson['id']
+
+def getUserID():
+    headers = {"Authorization": "{} {}".format(TOKEN_TYPE, ACCESS_TOKEN)}
+    response = requests.get("https://api.spotify.com/v1/me", headers=headers)
+    rJson = response.json()
+    return rJson['id']
+
+def getDiscoverTracks(userID, discoverID):
+    headers = {"Authorization": "{} {}".format(TOKEN_TYPE, ACCESS_TOKEN)}
+    userID = "spotify" # this is only overwritten because discover playlists are weird
+    response = requests.get("https://api.spotify.com/v1/users/{}/playlists/{}/tracks" \
+                            .format(userID, discoverID), headers=headers)
+    #print (response.url)
+    rJson = response.json()
+    return rJson
+
 @app.route('/callback')
 def callback():
     authToken = flask.request.args['code']
@@ -61,14 +102,29 @@ def callback():
     response = requests.post("https://accounts.spotify.com/api/token",
                              data=data)
     rJson = response.json()
+    #print (rJson)
     tokenType = rJson['token_type']
+    global TOKEN_TYPE
+    TOKEN_TYPE = tokenType
     accessToken = rJson['access_token']
+    global ACCESS_TOKEN
+    ACCESS_TOKEN = accessToken
     refreshToken = rJson['refresh_token']
+    global REFRESH_TOKEN
+    REFRESH_TOKEN = refreshToken
 
-    headers = {"Authorization": "{} {}".format(tokenType, accessToken)}
-    response = requests.get("https://api.spotify.com/v1/me", headers=headers)
-    print (response.json())
+    # need to get playlist id for discover playlist
+    # then user id
+    # then do something with the track data points
+
+    discoverID = getDiscoverID()
+    print (discoverID)
+    userID = getUserID()
+    print (userID)
+    tracksJson = getDiscoverTracks(userID, discoverID)
+
+    return str(tracksJson)
 
 if __name__ == "__main__":
     print ("Starting spotify client")
-    app.run(debug=True, port=PORT)
+    app.run(port=PORT)
